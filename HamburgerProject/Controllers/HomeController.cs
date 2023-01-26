7f5ae37c3b62dc4748248b1f498eb5c47e00f489
Onur Kırık <kirikonurr@gmail.com>
2023-01-26 04:26:00 +0300

    Add new menu trasnsaction is completed.

--- HamburgerProject/Controllers/HomeController.cs
+++ HamburgerProject/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using HamburgerProject.Models;
+﻿using HamburgerProject.DATA;
+using HamburgerProject.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System.Diagnostics;
 
 namespace HamburgerProject.Controllers
@@ -7,16 +9,45 @@ namespace HamburgerProject.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
             return View();
         }
+        
+
+        public IActionResult Menu()
+        {
+            var menus = _db.Menus.OrderBy(m => m.Id).ToList();
+
+            return View(menus);
+        }
+
+        public IActionResult CreateMenu()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateMenu(Menu menu)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Menus.Add(menu);
+                _db.SaveChanges();
+                return RedirectToAction("Menu");
+            }
+
+            return View();
+        }
+        
 
         public IActionResult Privacy()
         {
---
+++ HamburgerProject/DATA/Migrations/20230126011728_ChangedNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HamburgerProject.DATA.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MenuName",
+                table: "Menus",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MenuName",
+                table: "Menus",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
